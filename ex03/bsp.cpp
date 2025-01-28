d116bda9ad612bcb8b7c8fbc9b560e6978aff4a4
mpietrza <mpietrza@student.42barcelona.com>
2025-01-28 16:32:24 +0100

    cleaning done

--- ex03/bsp.cpp
+++ ex03/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: mpietrza <mpietrza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 14:21:02 by mpietrza          #+#    #+#             */
-/*   Updated: 2025/01/28 16:01:38 by mpietrza         ###   ########.fr       */
+/*   Updated: 2025/01/28 16:31:52 by mpietrza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,8 +65,6 @@ bool bsp(Point const a, Point const b, Point const c, Point const point)
 	d1 = sign(point, a, b);
 	d2 = sign(point, b, c);
 	d3 = sign(point, c, a);
-
-	//std::cout << "d1: " << d1 <<  ", d2: " << d2 << ", d3: " << d3 << std::endl;
 
 	has_neg = (d1 < 0) || (d2 < 0) || (d3 < 0);
 	has_pos = (d1 > 0) || (d2 > 0) || (d3 > 0);
